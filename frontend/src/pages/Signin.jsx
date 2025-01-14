4e391c3cf87333de83daffd57d99d9b0d79cfc36
0mRai <mymail.omnarainrai@gmail.com>
2025-01-14 05:54:15 +0530

    new components added

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,8 @@
+import {Button} from "../components/Button";
+export const Signin = ()=>{
+    return <div className="bg-slate-300 h-screen flex justify-center">
+       <div className="pt-4">
+          <Button label={"Sign in"} />
+        </div>
+    </div>
+}
